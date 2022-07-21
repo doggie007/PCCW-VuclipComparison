aca4e2ff8ebea7b65956523c776a32aac7fe7c75
James Tcheng <james.tcheng1234@gmail.com>
2022-07-21 18:22:50 +0800

    Initialised dashboard

--- website/cms-app/src/App.js
+++ website/cms-app/src/App.js
@@ -1,35 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-import * as React from "react";
+// import * as React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Dashboard from "./components/dashboard/Dashboard";
 import Button from "@mui/material/Button";
+import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<Button variant="contained">Hello World</Button>
-		</div>
+		<Router>
+			<div>
+				<Dashboard></Dashboard>
+			</div>
+		</Router>
 	);
 }
 
